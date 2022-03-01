fae3d6d1796d72d7462f1a4786390ad562850636
Shane Donaghy <shanedon2017@gmail.com>
2022-03-01 14:55:09 +0000

    cleaned up maze so 2d array can be filled automatically

--- Maze.py
+++ Maze.py
@@ -22,34 +22,47 @@ screen = pygame.display.set_mode((400, 400))
  
 walls = [] # List to hold the walls
 
+class Maze():
 
-#Here the maze is created using a 2D array
-#This can later be modified to give a random 2D array with
-#random variables
-level = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],   #1
-         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   #2
-         [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],   #3
-         [1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],   #4
-         [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],   #5
-         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],   #6
-         [1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1],   #7
-         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],   #8
-         [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],   #9
-         [1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1],   #10
-         [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],   #11
-         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],   #12
-         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]   #13
+    #Here the maze is created using a 2D array
+    #This can later be modified to give a random 2D array with
+    #random variables
+    level = np.full((11, 11), 0)  # random values
+
+    i = 0
+    j = 0
+
+    rows = len(level)
+
+    cols = len(level[0])
+
+    
+
+    print(cols)
+
+    while i < rows:
+        level[i][0] = 1
+        level[0][i] = 1
+        level[i][cols - 1] = 1
+        level[rows - 1][i] = 1
+
+        i += 1
+        if i == rows:
+            break
+
+        print(level)
 
 
-x = y = 0
-for row in level:
-    for col in row:
-        if col == 1:
-            Wall((x, y))
-        x += 16
-    y += 16
-    x = 0
- 
+        x = y = 0
+        for row in level:
+            for col in row:
+                if col == 1:
+                    Wall((x, y))
+                x += 16
+            y += 16
+            x = 0
+
+
 running = True
 while running:
     
@@ -64,7 +77,7 @@ while running:
     # Feom here we will draw the scene
     screen.fill((0, 0, 0))
     for wall in walls:
-        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)# Determine the wall what the wall looks like
     pygame.display.flip()
  
 pygame.quit()
