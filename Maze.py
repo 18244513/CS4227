e69b740fc921f7a49eac5f8a92c62d8d9324d330
Shane Donaghy <shanedon2017@gmail.com>
2022-03-03 16:02:00 +0000

    filled page to 960, 720

--- Maze.py
+++ Maze.py
@@ -18,7 +18,7 @@ os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
  
 # here we set up the display
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((960, 720))
  
 walls = [] # List to hold the walls
 
@@ -27,7 +27,9 @@ class Maze():
     #Here the maze is created using a 2D array
     #This can later be modified to give a random 2D array with
     #random variables
-    level = np.full((21, 21), 0)  # random values
+
+
+    level = np.full((45, 60), 0)  # random values
 
     i = 0
     j = 0
@@ -38,13 +40,15 @@ class Maze():
 
     while i < rows:
         level[i][0] = 1
-        level[0][i] = 1
         level[i][cols - 1] = 1
-        level[rows - 1][i] = 1
 
         i += 1
-        if i == rows:
-            break
+
+    while j < cols:
+        level[0][j] = 1
+        level[rows - 1][j] = 1
+        j += 1
+
 
 
 
@@ -67,8 +71,29 @@ class Maze():
         y += 16
         x = 0
 
+
+
+    def isPath(level):
+        rows = 10
+        cols = 15
+        for i in range(1, rows):
+            if (level[i][0] != 1):
+                level[i][0] = level[i-1][0]
+
+        for j in range(1, cols):
+            if (level[0][j] != 1):
+                level[0][j] = level[0][j-1]
+
+        return (level[rows - 1][cols - 1] == 1)
+
+
+
     print(level)
-    
+
+    if (isPath(level)):
+        print("Yes")
+    else:
+	    print("No")
 
 
 running = True
